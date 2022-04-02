95f68624bc6772fdc9827dc743f8f2aa01736c61
heeheejj <64790176+heeheejj@users.noreply.github.com>
2022-04-02 12:27:09 +0000

    3581: 소수

---
+++ boj/math/2581.py
@@ -0,0 +1,27 @@
+# 소수
+# 8:46 ~ 8:58
+#
+
+def isPrimeNumber(x):
+  if x == 1:
+    return False
+  
+  for i in range(2, x):  # x == 2이면 for문 안돌고 바로 return True
+    if x % i == 0:
+      return False
+  return True
+  
+m = int(input())
+n = int(input())
+sum = 0
+min = 0
+for i in range(n, m-1, -1):
+  if isPrimeNumber(i):  # 만약 소수이면
+    sum += i
+    min = i
+
+if sum == 0:
+  print(-1)
+else:
+  print(sum)
+  print(min)
