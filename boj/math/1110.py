180f257fd36e82b8119fc1951d9bf0204917134f
heeheejj <64790176+heeheejj@users.noreply.github.com>
2022-04-02 11:27:35 +0000

    1110: 더하기 사이클

---
+++ boj/math/1110.py
@@ -0,0 +1,20 @@
+# 더하기 사이클
+
+count = 0
+def func(x):
+  global count
+  if x == n and count != 0:
+    return
+    
+  count += 1
+  
+  if x < 10:
+    return func((x % 10)*10 + x) 
+  else:
+    return func((x % 10)*10 + (x // 10 + x % 10) % 10)
+    
+n = int(input())
+
+
+func(n)
+print(count)
