b1f4d29bfc505b1a3f33721d297dff1ea5ec0d78
heeheejj <64790176+heeheejj@users.noreply.github.com>
2022-04-04 16:13:29 +0000

    9613: GCD 합 - for문 + combinations 최적화 (2차원 리스트에서 안쪽 리스트의 크기가 모두 같고, 그 크기가 작을 때 사용)

--- boj/math/9613.py
+++ boj/math/9613.py
@@ -12,10 +12,11 @@ t = int(input())
 for i in range(t):
   sum = 0
   numbers = list(map(int, input().split()))
-  del numbers[0] 
-  combs = list(combinations(numbers, 2))
-  for j in range(len(combs)):
-    gcd = math.gcd(combs[j][0], combs[j][1])
+  del numbers[0]
+  
+  for x, y in combinations(numbers, 2):
+    # 이런 방식의 for문은 2차원 리스트에서 안쪽 리스트의 크기가 모두 같고, 그 크기가 작을 때 사용한다. for과 in 사이에 쓰는 변수의 개수는 안쪽 리스트의 크기이다.
+    gcd = math.gcd(x, y)
     sum += gcd
       
   result.append(sum)
