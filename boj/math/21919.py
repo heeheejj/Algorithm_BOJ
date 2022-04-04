79e684d7eafd8047829c31da438ea4c0e9a428fe
heeheejj <64790176+heeheejj@users.noreply.github.com>
2022-04-04 16:38:53 +0000

    21919: 소수 최소 공배수 - 백준에서 출력 초과가 뜸 -> 소수가 중복일 수 있다는 것 고려안해서 리스트에서 중복을 제거해줘서 정답! list(set(리스트)) 해주면 된다. / isPrimeNumber의 for문 최적화(제곱근까지로 변경)

---
+++ boj/math/21919.py
@@ -0,0 +1,27 @@
+# 소수 최소 공배수
+# 9: 39~9:48 -> 시간 초과
+# isPrimeNumber의 for문 최적화 (제곱근까지로 변경)
+# 백준에서 출력 초과가 뜸 -> 소수가 중복일 수 있다는 것 고려안해서 리스트를 set으로 감싸고 다시 list로 바꿔줘서 정답!
+
+import math
+
+def isPrimeNumber(x):
+  if x == 1 or x == 0:
+    return False
+  
+  for i in range(2, int(math.sqrt(x)) + 1):  # x == 2이면 for문 안돌고 바로 return True
+    if x % i == 0:
+      return False
+  return True
+
+n = int(input())
+inputs = list(set(list(map(int, input().split()))))
+result = 1
+for i in inputs:
+  if isPrimeNumber(i):
+    result *= i
+
+if result == 1:
+  print(-1)
+else:
+  print(result)
