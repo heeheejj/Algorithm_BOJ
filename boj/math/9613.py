fd8e491c4a841b440062e128238646176a0b17cf
heeheejj <64790176+heeheejj@users.noreply.github.com>
2022-04-05 13:56:13 +0000

    modified 9613: GCD 합

--- boj/math/9613.py
+++ boj/math/9613.py
@@ -15,7 +15,7 @@ for i in range(t):
   del numbers[0]
   
   for x, y in combinations(numbers, 2):
-    # 이런 방식의 for문은 2차원 리스트에서 안쪽 리스트의 크기가 모두 같고, 그 크기가 작을 때 사용한다. for과 in 사이에 쓰는 변수의 개수는 안쪽 리스트의 크기이다.
+    # 이런 방식의 for문은 2차원 리스트에서 안쪽 리스트의 크기가 모두 같고, 그 크기가 작을 때 사용한다. for과 in 사이에 쓰는 변수는 안쪽 리스트의 크기가 n이면 n개 써준다. 
     gcd = math.gcd(x, y)
     sum += gcd
       
