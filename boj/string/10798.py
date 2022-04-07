1af1447a102772c71a5785dbe64245f62da2e255
heeheejj <64790176+heeheejj@users.noreply.github.com>
2022-04-07 17:19:58 +0000

    10798: 세로읽기

--- boj/string/10798.py
+++ boj/string/10798.py
@@ -3,9 +3,18 @@ import sys
 
 input = sys.stdin.readline
 inputs = list()
-result = list()
+result = ""
 
 for i in range(5):
-  inputs = input().split()
+  inputs.append(input().rstrip())
 
-print(len(max(inputs, key = len)))
+max_length = len(max(inputs, key = len))
+
+for j in range(max_length):
+  t_string = ""
+  for i in range(5):
+    if (len(inputs[i]) - 1) >= j:  # inputs[i] 문자열의 마지막 인덱스가 j보다 작으면 pass
+      t_string += inputs[i][j]
+  result += t_string
+
+print(result)
