426acbf64263fd559b583068dab0de7d4cf67ebb
heeheejj <64790176+heeheejj@users.noreply.github.com>
2022-04-07 18:24:58 +0000

    9046: 복호화

--- boj/string/9046.py
+++ boj/string/9046.py
@@ -4,10 +4,20 @@ import sys
 
 input = sys.stdin.readline 
 t = int(input())
-inputs = list()
-result = list()
+input_string = ""
+
+alp = 'abcdefghijklmnopqrstuvwxyz'
 
 for i in range(t):
-  inputs = input().split()
-
-print(max(len(inputs)))
+  input_string = input().rstrip()
+  frequency = [0] * 26
+  for alphabet in input_string:
+    try:
+      frequency[alp.index(alphabet)] += 1
+    except:
+      continue
+  mf = max(frequency)
+  if frequency.count(mf) == 1:
+    print(alp[frequency.index(mf)])
+  else:
+    print("?")
