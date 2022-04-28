2093625b438092b6649fe900e222ce91d4e5b152
heejoo Jeong <64790176+heeheejj@users.noreply.github.com>
2022-04-28 02:07:10 +0000

    2504: 괄호의 값 푸는중..

--- boj/ds/2504.py
+++ boj/ds/2504.py
@@ -1,18 +1,30 @@
 # 괄호의 값
+# test case: ]], ()(]]
+# 반례: ()[[()]()()]()
 
 exp = ""  # expression:수식
 stack = list()
 flag = False # 이전 괄호가 닫힘 괄호일 때 True
-isLast = False  # x가 input()의 마지막인지 아닌지
-for x in input():
+last = ""
+
+inputs = input()
+if not inputs.count("(") == inputs.count(")") and inputs.count("[") == inputs.count("]"):
+  print("0")
+  exit()
+for x in inputs:
   if x == '(' or x == '[':  # 열림 괄호일 때
     stack.append(x)
     if flag:  # 이전 괄호가 닫힘 괄호이고 현재 열림 괄호일 때
       exp += "+"
       print(exp)
     flag = False
+    last = x
   else:  # 닫힘 괄호일 때
-    last = stack.pop()
+    if stack:
+      stack.pop()
+    else:  # 스택에 아무것도 없는데 닫힘괄호가 나오면 올바르지 못한 괄호열
+      print("0")
+      exit()
     if last == '(' and x == ')':
       exp += "2"
       print(exp)
@@ -26,12 +38,12 @@ for x in input():
       else:   # 이전 괄호가 ")"든 "]"든 닫힘이었고, 현재 "]" 괄호일 때
         exp += "*3"
         print(exp)
+      exp = str(eval(exp))  # exp = "(" + exp + ")" 이랑 같은건데 그냥 괄호를 미리 계산해줌
+      print(exp)
     else: # 닫힘괄호인데 (뒤에 바로 ]가 오거나 [뒤에 바로 )가 오게된 경우일 때 입력이 올바르지 못한 괄호열
       print("0")
       exit()
-    if not stack:  # 스택이 비어있다면 현재까지 모든 괄호가 짝이 맞춰져 없어진 것. "(", ")"를 수식 앞뒤에 추가
-      exp = "(" + exp + ")"
-      print(exp)
     flag = True
+    last = x
 
-print(exp)
+print(eval(exp))
