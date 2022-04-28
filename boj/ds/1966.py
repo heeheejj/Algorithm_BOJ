70b316f49e5c5cf0065e9b89158e508fef51d0c1
heejoo Jeong <64790176+heeheejj@users.noreply.github.com>
2022-04-28 13:39:38 +0000

    1966: 프린터 큐

--- boj/ds/1966.py
+++ boj/ds/1966.py
@@ -5,39 +5,23 @@
 import sys
 from collections import deque
 
-def printer(q, m):
-  result = 1
-  idx = m
-  while True:
-    print("mddj", max(list(q)))
-    print("q[idx]", q[idx]," idx", idx)
-    max_q = max(list(q))
-    if idx == 0 & max_q == q[idx]:
-      print(result)
-      break
-    if q[0] == max_q:
-      q.popleft()
-      result += 1
-      if idx != 0:
-        idx -= 1
-      else:  # 타겟 문서가 첫번째에 왔지만 맨뒤로 보내져야 하는 경우
-        idx = len(q)
-    else:
-      q.append(q.popleft())
-
-      # 다른 문서가 뒤로 감으로써 타겟문서 인덱스 옮겨짐
-      if idx != 0:
-        idx -= 1
-      else:  
-        idx = len(q) - 1
 input = sys.stdin.readline
 
 k = int(input())  # 테스트케이스의 수
 for _ in range(k):
   n, m = map(int, input().split())  # n: 문서의 개수, m: 타켓문서의 처음 인덱스(0부터)
   q = deque(map(int, input().split()))
-  print("q",q)
-  if n == 1:
-    print("1")
-  else:
-    printer(q, m)
+  idx = deque([x for x in range(0, n)])
+  order = 1
+  while True:
+    if q[0] == max(q):
+      if idx[0] == m:
+        print(order)
+        break
+      else:
+        q.popleft()
+        idx.popleft()
+        order += 1
+    else:  # 큐의 첫번째 원소가 가장 큰 원소가 아니면 맨뒤로 보내기
+      q.append(q.popleft())
+      idx.append(idx.popleft())
--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-temp = "(1231)"
-print(temp[:0]+temp[1:5])
+a = [1, 2, 3]
+b = a
+a = [3, 1, 0]
+print(a)
+print(b)
+
+a = [1, 2, 3]
+b = a
+a.append(8)
+print(a)
+print(b)
