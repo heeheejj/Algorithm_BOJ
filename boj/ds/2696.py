ba226fb32629d947856d111344b9dead6f0d1f7b
heejoo Jeong <64790176+heeheejj@users.noreply.github.com>
2022-05-06 04:13:10 +0000

    2696: 중앙값 구하기

--- boj/ds/2696.py
+++ boj/ds/2696.py
@@ -14,18 +14,28 @@ def getMiddle(inputs):  #입려값 전체를 파라미터로 받음
   # 2에서 중앙값이 나오면 2에서 pop해서 middle로 만들고 현재값을 2로 push 
   # 1에서 중앙값이 나오면 1에서 pop해서 middle로 만들고 현재값을 1로 push  
   middle = inputs[0]  # 첫 번째 중앙값(데이터 하나일 때)(min_heap, max_heap 없이 구할 수 있음)
+
+  print(middle, end = ' ')
   
   for i, x in enumerate(inputs[1:], start = 1):
     if x > middle:
-      heapq.heappush(right_heap, x)
+      heapq.heappush(left_heap, x)
     else:
-      heapq.heappush(left_heap, -x)
+      heapq.heappush(right_heap, (-x, x))
 
     if i % 2 == 0: #홀수번째 수일때(=i는 짝수)
       if len(right_heap) < len(left_heap):
-        heapq.heappush(right_heap, middle)
+        heapq.heappush(right_heap, (-middle, middle))
         middle = heapq.heappop(left_heap)
-        # 하다 말았어요.....
+      elif len(right_heap) > len(left_heap):
+        heapq.heappush(left_heap, middle)
+        middle = heapq.heappop(right_heap)[1]
+
+      print(middle, end = ' ')   
+    if (i+1) % 20 == 0 or i == len(inputs) - 1:
+      print()
+      
+      
     
 input = sys.stdin.readline
 
@@ -36,7 +46,7 @@ for _ in range(t):
   inputs = []
   print(math.ceil(m / 2))
   if m % 10 == 0:
-    for _ in range(m // 10 + 1):
+    for _ in range(m // 10):
       inputs.extend(list(map(int, input().split())))
   else:
     for _ in range(m // 10 + 1):
