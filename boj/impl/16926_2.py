8c05904736dc3ada92a0cceba5883e88b4dc6454
heejoo Jeong <64790176+heeheejj@users.noreply.github.com>
2022-06-02 12:05:34 +0000

    16926: 배열 돌리기 1

--- boj/impl/16926_2.py
+++ boj/impl/16926_2.py
@@ -1,16 +1,13 @@
 # 배열 돌리기 1
-# 큐 사용
+# 시간 초과
 
 import sys
-from collections import deque
 
 def rotate(level):
   nx, ny = level, level
   dx = [1, 0, -1, 0]
-  dy = [0, 1, 0, -1] 
-
-  queue = deque()
-  global arr
+  dy = [0, 1, 0, -1]
+  result = [row[:] for row in arr]  
   
   for i in range(4):
     while True:
@@ -18,26 +15,17 @@ def rotate(level):
         break
       nx += dx[i]
       ny += dy[i]
-      queue.append(arr[nx - dx[i]][ny - dy[i]])
-  queue.rotate(r)
+      result[nx][ny] = arr[nx - dx[i]][ny - dy[i]]
+  return result
 
-  nx, ny = level, level
-  for i in range(4):
-    while True:
-      if not ((0+level) <= nx + dx[i] < (n-level) and (0+level) <= ny + dy[i] < (m-level)):
-        break
-      arr[nx][ny] = queue.popleft()
-      nx += dx[i]
-      ny += dy[i]
-
-sys.stdin = open("input.txt","r")
 input = sys.stdin.readline
 
 n, m, r = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
 
-for l in range(min(n, m) // 2):
-  rotate(l)
+for r in range(r):
+  for l in range(min(n, m) // 2):
+    arr = rotate(l)
 
 for i in range(n):
   for j in range(m):
