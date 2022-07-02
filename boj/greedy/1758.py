9e8b2ff251c3b4b6d6eb66195436b19d8b1c29e8
heejoo Jeong <64790176+heeheejj@users.noreply.github.com>
2022-07-02 15:45:22 +0000

    1758: 알바생 강호

--- boj/greedy/1758.py
+++ boj/greedy/1758.py
@@ -7,13 +7,13 @@ N = int(input())
 money = list()
 
 for i in range(N):
-  money.append((int(input()), i))
+  money.append(int(input()))
 
 money.sort(reverse = True)
 
 tip = 0
 for i in range(N):
-  temp = money[i][0] - i
+  temp = money[i] - i
   if temp > 0:
     tip += temp
 
